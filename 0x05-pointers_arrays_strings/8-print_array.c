367b132c88cec280d90966694196e67af4ba180e
zineb <zanobazaaboul3@gmail.com>
2023-05-02 09:35:15 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - a function that prints n elements of an array
@@ -6,6 +7,7 @@
  * @n: is the number of elements OF the array to be printed
  * Return: a and n inputs
  */
+
 void print_array(int *a, int n)
 {
 	int i;
